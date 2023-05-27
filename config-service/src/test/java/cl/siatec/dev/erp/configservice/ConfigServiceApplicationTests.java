4fcc49a30f2918311a067cd3113875e7c41c6094
siatec <erp.siatec@gmail.com>
2023-05-27 12:45:48 -0400

    Commit init config-service

---
+++ config-service/src/test/java/cl/siatec/dev/erp/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.siatec.dev.erp.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
